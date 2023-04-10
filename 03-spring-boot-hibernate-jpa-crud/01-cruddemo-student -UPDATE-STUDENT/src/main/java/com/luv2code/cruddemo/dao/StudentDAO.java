13562aff56e685fb4362497c61fa49617bd2fe27
Darren Dhanpat <darren.dhanpat@yahoo.com>
2023-04-10 14:00:54 -0400

    added read and update methods

---
+++ 03-spring-boot-hibernate-jpa-crud/01-cruddemo-student -UPDATE-STUDENT/src/main/java/com/luv2code/cruddemo/dao/StudentDAO.java
@@ -0,0 +1,23 @@
+package com.luv2code.cruddemo.dao;
+
+import com.luv2code.cruddemo.entity.Student;
+
+import java.util.List;
+
+public interface StudentDAO {
+
+    void save(Student theStudent);
+
+    //add new method to interface for retrieving Primary key
+    Student findById(Integer id);
+
+    //New method to return list of students
+    List<Student> findAll();
+
+    //New method to find by last name
+    List<Student> findByLastName(String theLastName);
+
+    //Add method to UPDATE Student object
+    void update(Student student);
+
+}
